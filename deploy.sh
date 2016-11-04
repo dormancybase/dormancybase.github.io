007d2963cdcce2b95b90b3c9b3d1b33a832a0052
David Fazekas <fazekasda@gmail.com>
2016-11-04 19:24:14 +0100

    automatic deploy

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-invoke template.deploy
-invoke database.deploy
-invoke dist_clone
-invoke deploy
-invoke clean_all
--- template/main.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by fazekasda on 2016.06.28..
- */
-import cytoscape from 'cytoscape';
-
-console.log('is this thing on?');
